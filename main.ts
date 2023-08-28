9ae6a28940631fe067f458598ac5b726dec7cafc
oeyoews <jyao4783@gmail.com>
2023-08-28 11:28:32 +0800

    Automatic commit

--- main.ts
+++ main.ts
@@ -3,12 +3,14 @@ import SelectionSort from '@/src/SelectionSort';
 
 /*
  * 生成随机数组
- * 实例化sort, 并且同时传入随机数组参数
  */
 const array = Array.from({ length: 5 }, () => Math.floor(Math.random() * 100));
 const expectedSortedArray = [...array].sort((a, b) => a - b);
 console.log(`排序前: ${array.join()}`);
 
+/*
+ * 测试
+ */
 const bubbleSort = new BubbleSort(array);
 const selectionSort = new SelectionSort(array);
 
@@ -19,7 +21,9 @@ selectionSort.sort();
  * 输出排序后的数组
  */
 console.log(`排序后: ${array.join()}`);
-// 检查排序后的数组是否和预期结果相等
+
+/*
+ * 检查排序后的数组是否和预期结果相等
+ */
 const isEqual = JSON.stringify(array) === JSON.stringify(expectedSortedArray);
 console.log(`排序结果是否正确: ${isEqual ? '是' : '否'}`);
-console.log();
