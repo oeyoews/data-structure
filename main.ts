40ff77225460470adef6d2b5ae3748967ecb79d7
oeyoews <jyao4783@gmail.com>
2023-08-29 16:48:48 +0800

    feat: support prompts

--- main.ts
+++ main.ts
@@ -1,15 +1,37 @@
 import check from '@/lib/check';
+import prompts from 'prompts';
 
 const array = Array.from({ length: 5 }, () => Math.floor(Math.random() * 100));
-/*
- * 测试
- */
 
 async function sort(sortType: SortType) {
   const module = await import(`@/src/${sortType}`);
   new module.default(array).sort();
 }
 
-sort('ShellSort').then(() => {
+// 使用 prompts 提示用户选择排序方法
+async function promptForSortType() {
+  const choices = [
+    'BubbleSort',
+    'SelectionSort',
+    'InsertionSort',
+    'QuickSort',
+    'ShellSort',
+  ].map((sortType) => ({
+    title: sortType.replace('Sort', ''),
+    value: sortType,
+  }));
+  const response = await prompts({
+    type: 'select',
+    name: 'sortType',
+    message: '请选择排序方法:',
+    choices,
+  });
+
+  const selectedSortType = response.sortType as SortType;
+  await sort(selectedSortType);
   check(array);
+}
+
+promptForSortType().catch((error) => {
+  console.error('出现错误:', error);
 });
